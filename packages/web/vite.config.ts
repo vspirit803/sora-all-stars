b9a0860e5643caf7fb29f19d7022932825c57ac1
vspirit803 <lx546488173@gmail.com>
2023-06-04 22:42:07 +0800

    chore: 升级vite版本

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,12 +1,13 @@
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@core": "@sora-all-stars/core/src",
+      "@core": path.resolve("../core/src/"),
     },
   },
   server: {
